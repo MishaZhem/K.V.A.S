0fea648d01872309c06262fd17e060454be9b7de
Elkopasto <Elkopasto@gmail.com>
2025-10-04 21:46:39 +0200

    + New login page, ~ fixed map colors

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -2,32 +2,33 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import DriverLoginWidget from "./components/DriverLogin";
+import Login from "./pages/Login";
 import { useState } from "react";
 import type { UserContextType } from "./types/userContext";
 import { Map } from "./pages";
 
 const Router = () => {
-    const loading = false;    
-    const [userContext, setUserContext] =  useState<UserContextType | undefined>(undefined);
-    // Routing configuration
-    const routing = createBrowserRouter(
-        [
-            {
-                path: "/",
-                element: <DriverLoginWidget updateUserContext={(c) => setUserContext(c)} />,
-            },
-            {
-                path: "/app",
-                element: <Map user={userContext}/>
-            }
-        ],
-    );
-    if (userContext) {
-        return <Map user={userContext} />
-    } else {
-        return <DriverLoginWidget updateUserContext={(c) => setUserContext(c)} />
-    }
-    // return loading ? <p></p> : <RouterProvider router={routing} />;
+  const loading = false;
+  const [userContext, setUserContext] = useState<UserContextType | undefined>(undefined);
+  // Routing configuration
+  const routing = createBrowserRouter(
+    [
+      {
+        path: "/",
+        element: <Login updateUserContext={(c) => setUserContext(c)} />,
+      },
+      {
+        path: "/app",
+        element: <Map user={userContext} />
+      }
+    ],
+  );
+  if (userContext) {
+    return <Map user={userContext} />
+  } else {
+    return <Login updateUserContext={(c) => setUserContext(c)} />
+  }
+  // return loading ? <p></p> : <RouterProvider router={routing} />;
 };
 
 export default Router
