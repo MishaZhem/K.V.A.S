a81ed4eeffe21221bb197b8e7e07d22bae335c0f
MishaZhem <mz-100@mail.ru>
2025-10-04 20:51:36 +0200

    chore: make normal font

--- frontend/src/components/Profile.tsx
+++ frontend/src/components/Profile.tsx
@@ -1,14 +1,14 @@
 import type { UserContextType } from "../types/userContext";
 
-const Profile = ({userContext, shown}: {userContext: UserContextType, shown: boolean}) => {
+const Profile = ({ userContext, shown }: { userContext: UserContextType, shown: boolean }) => {
     return shown ? (
-        <div className="text-[#ABABAB] overflow-y-auto max-h-1/2 rounded-3xl w-1/8 bg-[#171717] absolute border-3 border-[#434343] right-10 top-30 p-2 pl-3 shadow-xl/30">
+        <div className="text-[#ABABAB] overflow-y-auto grow max-h-1/2 rounded-3xl bg-[#171717] absolute border-3 border-[#434343] right-10 top-30 p-2 pl-3 shadow-xl/30">
             <h2>Profile and settings</h2>
             <hr></hr>
             <p>Name: {userContext.username}</p>
             <button>&gt; Change name</button>
             <hr></hr>
-            <p>You are registered as a {userContext.isCourier ? "courier" : "driver"} at Uber.</p>
+            <p>You are registered as a {userContext.isCourier ? "courier" : "driver"} <br /> at Uber.</p>
             <button>&gt; Change registration</button>
         </div>
     ) : <></>
