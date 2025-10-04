81720ce06aa1d3579288da8906a534071fb6e6ee
MishaZhem <mz-100@mail.ru>
2025-10-04 22:39:17 +0200

    chore: add bottom line

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,4 +1,6 @@
 import user from "./svg/user.svg";
 import uber from "./svg/uber.svg";
+import points from "./svg/points.svg";
+import ButtonSleepImg from "./svg/buttonSleep.svg";
 
-export { user, uber };
+export { user, uber, points, ButtonSleepImg };
