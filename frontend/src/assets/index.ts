457adafbfb175a7decb2c0e4aedfaf3fecf58420
MishaZhem <mz-100@mail.ru>
2025-10-05 02:55:32 +0200

    chore: add bottom menu for  map

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -3,5 +3,15 @@ import uber from "./svg/uber.svg";
 import points from "./svg/points.svg";
 import ButtonSleepImg from "./svg/buttonSleep.svg";
 import ButtonMoneyImg from "./svg/buttonMoney.svg";
+import ButtonSleepSmImg from "./svg/buttonSleepSM.svg";
+import ButtonMoneySmImg from "./svg/buttonMoneySM.svg";
 
-export { user, uber, points, ButtonSleepImg, ButtonMoneyImg };
+export {
+  user,
+  uber,
+  points,
+  ButtonSleepImg,
+  ButtonMoneyImg,
+  ButtonMoneySmImg,
+  ButtonSleepSmImg,
+};
--- frontend/src/components/ButtonSleep.tsx
+++ frontend/src/components/ButtonSleep.tsx
@@ -1,8 +1,13 @@
-import { ButtonSleepImg } from "../assets";
+import { ButtonSleepImg, ButtonSleepSmImg } from "../assets";
 
-const ButtonSleep = () => {
+interface ButtonProps {
+    start: number
+    end: number
+};
+
+const ButtonSleep = (props: ButtonProps) => {
     return (
-        <div className="relative w-full max-w-[500px] h-15 bg-gray-900 overflow-hidden backdrop-blur-md">
+        <div className="relative w-full h-15 bg-gray-900 overflow-hidden backdrop-blur-md">
             <div className="absolute inset-0 flex items-center justify-center">
                 <div className="absolute inset-0    ">
                     <div className="absolute top-0 left-0 right-0 bottom-0 border-1 border-teal-600"></div>
@@ -16,22 +21,31 @@ const ButtonSleep = () => {
                     <div className="absolute inset-x-4 w-1/4 centerAbsoluteX bottom-0 h-2 border-b-2 border-teal-400"></div>
                 </div>
 
-                <div className="backLinesGreen"></div>
+                {props.end - props.start > 3 ? (
+                    <>
+                        <div className="backLinesGreen"></div>
+                    </>
+                ) : ""}
 
-                <div className="absolute inset-4 flex justify-between items-center">
-                    <div className="flex gap-1 items-center opacity-55">
-                        <div className="w-[2px] h-10 bg-teal-400"></div>
-                        <div className="w-[2px] h-6 bg-teal-400"></div>
-                        <div className="w-20 h-[2px] bg-teal-400"></div>
-                    </div>
-                    <div className="flex gap-1 items-center opacity-55">
-                        <div className="w-20 h-[2px] bg-teal-400"></div>
-                        <div className="w-[2px] h-6 bg-teal-400"></div>
-                        <div className="w-[2px] h-10 bg-teal-400"></div>
-                    </div>
-                </div>
+                {props.end - props.start > 3 ? (
+                    <>
+                        <div className="absolute inset-4 flex justify-between items-center">
+                            <div className="flex gap-1 items-center opacity-55">
+                                <div className="w-[2px] h-10 bg-teal-400"></div>
+                                <div className="w-[2px] h-6 bg-teal-400"></div>
+                                <div className="w-20 h-[2px] bg-teal-400"></div>
+                            </div>
+                            <div className="flex gap-1 items-center opacity-55">
+                                <div className="w-20 h-[2px] bg-teal-400"></div>
+                                <div className="w-[2px] h-6 bg-teal-400"></div>
+                                <div className="w-[2px] h-10 bg-teal-400"></div>
+                            </div>
+                        </div>
+                    </>
+                ) : ""}
+
                 {/* Иконки */}
-                <img src={ButtonSleepImg} alt="Button Sleep" className="relative z-10" />
+                <img src={props.end - props.start > 3 ? ButtonSleepImg : ButtonSleepSmImg} alt="Button Sleep" className="relative z-10" />
             </div>
         </div>
     );
